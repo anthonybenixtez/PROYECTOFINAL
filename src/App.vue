f09991c19844b0192676d684f224a64203dba062
M10Mozo <149447491+M10Mozo@users.noreply.github.com>
2024-10-14 20:35:05 -0600

    login

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <template>
-  <TopMenu />
+  <!-- Renderiza el Navbar solo si no estás en la página de inicio de sesión -->
+  <Navbar v-if="!isLoginPage" /> 
 
-  <RouterView />
-
-  <div></div>
+  <!-- Main -->
+  <main class="flex-1 overflow-auto">
+    <router-view />
+  </main>
 </template>
 
 <script setup lang="ts">
-import './style.css';
-import TopMenu from './modules/common/components/TopMenu.vue';
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
+import Navbar from './modules/common/components/TopMenu.vue';
+
+const route = useRoute();
+const isLoginPage = computed(() => route.name === 'paginaprincipal'); // Asegúrate de que coincida con el nombre de la ruta
 </script>
+
+<style scoped>
+/* Estilos generales */
+</style>
